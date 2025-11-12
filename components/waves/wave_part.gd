da76bdb247cc719a613139c9d0b793fd88c3ee32
Macmist <quentin.musy@gmail.com>
2025-11-12 17:06:13 +0000

    chore: fixed collision

--- components/waves/wave_part.gd
+++ components/waves/wave_part.gd
@@ -1,18 +1,27 @@
 extends RigidBody2D
 class_name WavePart
 
+@export var strength: int = 20
+@export var health: int = 30
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
 
 func hit():
 	queue_free()
 
 
 func _on_body_entered(body: Node) -> void:
-	print("entered collision, ", body)
 	if body is Tower:
-		print("it is a tower")
-		var destroyed = body.damage(20)
-		print("destroyed tower? ", destroyed)
+		print("touched a tower")
+		var touch = min(strength, health)
+		var destroyed = body.damage(touch)
 		if !destroyed: # The object was too strong and the wave gets destroyed
 			hit()
-		
-	pass # Replace with function body.
+		else:
+			health -= touch
+		if health <= 0:
+			hit()
+	else:
+		print("something else")
